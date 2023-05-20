c9ad769532d491996ba96de4f5ee99d3bed92060
Yilun Guan <zoom.aaron@gmail.com>
2023-05-20 19:33:28 +0000

    more timelines

--- web/src/app.py
+++ web/src/app.py
@@ -2,64 +2,40 @@ import streamlit as st
 from streamlit_timeline import st_timeline
 st.set_page_config(layout="wide")
 
+import datetime as dt
 from schedlib import policies, core
+from scheduler_server.configs import get_default_config
 import utils
 
-import os.path as op
-import datetime as dt
 
-minute = 60
-hour = 60 * minute
-
-config = {
-    'master_schedule': op.dirname(__file__) + '/schedule_sat.txt',
-    'rules': {
-        'sun-avoidance': {
-            'min_angle_az': 6,  # deg
-            'min_angle_alt': 6, # deg
-            'time_step': 10,    # sec
-            'n_buffer': 3,     # 30 * 1 = 0.5 mins
-        },
-        'day-mod': {
-            'day': 0,
-            'day_mod': 1,
-            'day_ref': dt.datetime(2014, 1, 1, 0, 0, 0, tzinfo=dt.timezone.utc),
-        },
-        'calibration-min-duration': {
-            'min_duration': 5 * minute,
-        },
-        'make-source-plan': {
-            'specs': [{'bounds_x': [-0.5, 0.5], 'bounds_y': [-0.5, 0.5]}],  # test
-            'spec_shape': 'ellipse',
-            'max_obs_length': 6000,
-        },
-        'make-source-scan': {
-            'preferred_length': 1800,
-        }
-    },
-    'calibration_targets': ["uranus", "saturn"],
-    'soft_targets': []
-}
+config = get_default_config('basic')
 policy = policies.BasicPolicy(**config)
 
 # =================
 # streamlit web
 # =================
 
-if "data" not in st.session_state:
-    st.session_state.data = []
-
-if "groups" not in st.session_state:
-    st.session_state.groups = []
+for k in ['data_orig', 'groups_orig', 'data_trans', 'groups_trans', 'data_merge', 'groups_merge']:
+    if k not in st.session_state:
+        st.session_state[k] = []
 
 def on_load_schedule():
     t0 = dt.datetime.combine(start_date, start_time).astimezone(dt.timezone.utc)
     t1 = dt.datetime.combine(end_date, end_time).astimezone(dt.timezone.utc)
+
     seqs = policy.init_seqs(t0, t1)
+    data, groups = utils.seq2visdata_flat(seqs)
+    st.session_state.data_orig = data
+    st.session_state.groups_orig = groups
+
     seqs = policy.transform(seqs)
     data, groups = utils.seq2visdata_flat(seqs)
-    st.session_state.data = data
-    st.session_state.groups = groups
+    st.session_state.data_trans = data
+    st.session_state.groups_trans = groups
+
+    seqs = policy.merge(seqs)
+    data = core.seq_map(utils.block2dict, seqs)
+    st.session_state.data_merge = data
 
 st.title("SO Scheduler Web")
 
@@ -78,4 +54,9 @@ with st.sidebar:
 
     st.button("Load Schedule", on_click=on_load_schedule)
 
-timeline = st_timeline(st.session_state.data, st.session_state.groups)
+st.markdown("## Initial Sequences")
+timeline_orig = st_timeline(st.session_state.data_orig, st.session_state.groups_orig, key='orig')
+st.markdown("## Transformed Sequences")
+timeline_trans = st_timeline(st.session_state.data_trans, st.session_state.groups_trans, key='trans')
+st.markdown("## Output Schedule")
+timeline_merge = st_timeline(st.session_state.data_merge, key='merge')
