e03211ad11e7faa80463fd2693d1bda00bdc9d6a
ykyohei <38639108+ykyohei@users.noreply.github.com>
2024-04-22 18:55:10 -0400

    clean up

--- src/schedlib/policies/__init__.py
+++ src/schedlib/policies/__init__.py
@@ -1,5 +1,4 @@
 from .flex import FlexPolicy
 from .sat import SATPolicy
-from .tsat import TSATPolicy
-from .satp3 import SATP3Policy
 from .satp1 import SATP1Policy
+from .satp3 import SATP3Policy
